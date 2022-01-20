41f43a49d33e5fd224a343993c6da035ffee7cd4
[yair-r] <yair7705778@gmail.com>
2022-01-20 20:39:22 +0300

    111

--- src/components/house.jsx
+++ src/components/house.jsx
@@ -1,5 +1,7 @@
 import React from "react"
 import PropTypes from "prop-types"
+// import { Link } from "react-router-dom";
+
 export default function House({ house }) {
   const { name, image, description, artist } = house
   return (
