c325dc897ae867c7dbe00f762a85623f5554075e
[yair-r] <yair7705778@gmail.com>
2022-01-20 12:42:51 +0300

    1

---
+++ src/components/main.jsx
@@ -0,0 +1,34 @@
+import { createContext, useState, useContext, useMemo } from "react"
+import Card from "../components/Card"
+const UserContext = createContext({
+  userName: "",
+  setUserName: () => {},
+})
+
+export default function Main() {
+  const [userName, setUserName] = useState("John Smith")
+  const value = useMemo(() => ({ userName, setUserName }), [userName])
+
+  return (
+    <UserContext.Provider value={value}>
+      <UserNameInput />
+      <UserInfo />
+      <Card />
+
+    </UserContext.Provider>
+  )
+}
+
+function UserNameInput() {
+  const { userName, setUserName } = useContext(UserContext)
+  const changeHandler = (event) => setUserName(event.target.value)
+
+  return <input type="text" value={userName} onChange={changeHandler} />
+}
+
+function UserInfo() {
+  const { userName } = useContext(UserContext)
+  return <span>{userName}</span>
+}
+
+ 
