25f66cffbd03bee47c5e7ba20ac2cc5010ba2f4e
[yair-r] <yair7705778@gmail.com>
2022-01-23 18:37:29 +0300

    111

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React from "react"
+import { NameForm } from "./components/nameForm"
+import { Main } from "./components/main"
+
+import { SingleHouse } from "./components/singleHouse"
+import { Route, Routes, BrowserRouter } from "react-router-dom"
+
+export function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" exact={true} element={<NameForm />} />
+        <Route path="/main" element={<Main />} />
+        <Route path="/houses/:id" element={<SingleHouse />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
