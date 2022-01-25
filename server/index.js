a375d5a278d377b5950cc22edcb63dae2bf4b760
[yair-r] <yair7705778@gmail.com>
2022-01-25 22:28:58 +0300

    111

--- server/index.js
+++ server/index.js
@@ -1,39 +1,35 @@
-const server = require('http').createServer()
-const io = require('socket.io')(server, {
+const server = require("http").createServer()
+const io = require("socket.io")(server, {
   cors: {
-    origin: '*'
-  }
+    origin: "*",
+  },
 })
 const log = console.log
 
-const registerMessageHandlers = require('./handlers/messageHandlers')
-// const registerUserHandlers = require('./handlers/userHandlers')
+const registerMessageHandlers = require("./handlers/messageHandlers")
 
 const onConnection = (socket) => {
-  log('User connected')
+  log("User connected")
 
   const { roomId } = socket.handshake.query
 
-  log('roomId=',roomId)
- 
+  log("roomId=", roomId)
 
   socket.roomId = roomId
 
   socket.join(roomId)
 
   registerMessageHandlers(io, socket)
-  // registerUserHandlers(io, socket)
 
-  socket.on('disconnect', () => {
-    log('User disconnected')
+  socket.on("disconnect", () => {
+    log("User disconnected")
     socket.leave(roomId)
   })
 }
 
-io.on('connection', onConnection)
+io.on("connection", onConnection)
 
 const PORT = process.env.PORT || 5000
 server.listen(PORT, () => {
   console.log(`Server ready. Port: ${PORT}`)
- 
 })
