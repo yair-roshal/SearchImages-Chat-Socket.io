6b8ae39f2cc665f14245bc7a8bc76e671c87ad14
[yair-r] <yair7705778@gmail.com>
2022-01-26 00:08:54 +0300

    222

--- client/src/components/ChatRoom/MessageList/MessageListItem.js
+++ client/src/components/ChatRoom/MessageList/MessageListItem.js
@@ -1,11 +1,11 @@
-import TimeAgo from "react-timeago" 
+import TimeAgo from "react-timeago"
 import Card from "@mui/material/Card"
 import ListItem from "@mui/material/ListItem"
 import CardHeader from "@mui/material/CardHeader"
 import CardContent from "@mui/material/CardContent"
- 
+
 export const MessageListItem = ({ msg }) => {
-  const {   messageText, senderName, createdAt  } = msg
+  const { messageText, senderName, createdAt } = msg
   return (
     <ListItem>
       <Card sx={{ width: "95%" }}>
