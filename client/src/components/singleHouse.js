ce10cc295c1b968d08610f61c6f66ed6ccdb4a7f
yair <yair7705778@gmail.com>
2022-02-02 14:32:07 +0200

    clean

--- client/src/components/singleHouse.js
+++ client/src/components/singleHouse.js
@@ -1,10 +1,10 @@
 import React from "react"
 import { useParams } from "react-router-dom"
-import { items } from "../data/data" 
-import { useLocalStorage, useChat } from "../hooks" 
+import { items } from "../data/data"
+import { useLocalStorage, useChat } from "../hooks"
 import { MessageForm } from "./ChatRoom/MessageForm"
 import { MessageList } from "./ChatRoom/MessageList"
-  
+
 export function SingleHouse() {
   const [username] = useLocalStorage("username")
 
@@ -20,28 +20,26 @@ export function SingleHouse() {
 
   const { roomId } = useParams()
   const housesAll = formatData(items)
-   const { artist, image, name } = housesAll[roomId] 
-   const { messages, sendMessage } = useChat(roomId)
+  const { artist, image, name } = housesAll[roomId]
+  const { messages, sendMessage } = useChat(roomId)
 
   return (
-    
-      <div className="wrapper">
-        <div className="column-1">
-          <article className="house"> 
-            <p className="house-title">{name}</p>
-            <p className="house-artist">{artist}</p>
-            <div className="img-container2">
-              <img src={image} alt="apartment house" />
-            </div>
-          </article>
-        </div>
-
-        <div className="column-2">
-          <h2 className="text-center">Chat</h2>
-          <MessageList messages={messages}   />
-          <MessageForm username={username} sendMessage={sendMessage} />
-        </div>
+    <div className="wrapper">
+      <div className="column-1">
+        <article className="house">
+          <p className="house-title">{name}</p>
+          <p className="house-artist">{artist}</p>
+          <div className="img-container2">
+            <img src={image} alt="apartment house" />
+          </div>
+        </article>
       </div>
-  
+
+      <div className="column-2">
+        <h2 className="text-center">Chat</h2>
+        <MessageList messages={messages} />
+        <MessageForm username={username} sendMessage={sendMessage} />
+      </div>
+    </div>
   )
 }
--- server/handlers/messageHandlers.js
+++ server/handlers/messageHandlers.js
@@ -1,48 +1,47 @@
-const { nanoid } = require('nanoid')
-const low = require('lowdb')
-const FileSync = require('lowdb/adapters/FileSync')
+const { nanoid } = require("nanoid")
+const low = require("lowdb")
+const FileSync = require("lowdb/adapters/FileSync")
 
-const adapter = new FileSync('db/messages.json')
+const adapter = new FileSync("db/messages.json")
 const db = low(adapter)
 
 db.defaults({
   messages: [
     {
-      messageId: '1',
-      userId: '1',
-      senderName: 'Bob',
-      messageText: 'What are you doing here?',
-      createdAt: '2021-01-14'
+      messageId: "1",
+      userId: "1",
+      senderName: "Bob",
+      messageText: "What are you doing here?",
+      createdAt: "2021-01-14",
     },
     {
-      messageId: '2',
-      userId: '2',
-      senderName: 'Alice',
-      messageText: 'Go back to work!',
-      createdAt: '2021-02-15'
-    }
-  ]
+      messageId: "2",
+      userId: "2",
+      senderName: "Alice",
+      messageText: "Go back to work!",
+      createdAt: "2021-02-15",
+    },
+  ],
 }).write()
 
 module.exports = (io, socket) => {
   const getMessages = () => {
-    const messages = db.get('messages').value()
-    io.in(socket.roomId).emit('messages', messages)
+    const messages = db.get("messages").value()
+    io.in(socket.roomId).emit("messages", messages)
   }
 
   const addMessage = (message) => {
-    db.get('messages')
+    db.get("messages")
       .push({
         messageId: nanoid(8),
         createdAt: new Date(),
-        ...message
+        ...message,
       })
       .write()
 
     getMessages()
   }
 
- 
-  socket.on('message:get', getMessages)
-  socket.on('message:add', addMessage)
- }
+  socket.on("message:get", getMessages)
+  socket.on("message:add", addMessage)
+}
